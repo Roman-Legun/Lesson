a84932c7660e223ef28e8a5cd2d39f0e14af7075
Роман Легун <legun.roman@yandex.ua>
2016-10-28 15:48:33 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -19,6 +19,11 @@ get '/users' do
 
 end
 
+get '/kino' do
+  @error = "сторiнка поки що не доступна!!!"
+  erb :kino
+end
+
 post '/users' do
 
   @user_name = params[:user_name]
@@ -27,6 +32,20 @@ post '/users' do
   @date = params[:date]
   @master = params[:master]
   @color = params[:color]
+
+  hh = {
+    :user_name => "Введiть ваше iм'я!",
+    :user_name2 => "Введiть вашу фамiлiю!",
+    :phone => "Введiть ваш номер телефону!",
+    :date => "Введiть вашу дату регiстрацii!",
+    :color => "Введiть ваш  колiр!"
+  }
+  hh.each do |key,value|
+    if params[key] == ""
+      @error = hh[key]
+      return erb :users
+    end
+  end
 
   erb "OK #{@user_name} #{@user_name2} ми радi що ви в нас зарегiструвалися!      
   Наш майстер #{@master} буде вас чекати на: #{@date} #{@color}"
